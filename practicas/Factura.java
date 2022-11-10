bb6624ab043f2ffc101392015ced66c847417a8a
alopgem153 <alopgem153@g.educaand.es>
2022-11-10 12:29:21 +0100

    He modificado e ficehero Factura.java con un printLN

--- practicas/Factura.java
+++ practicas/Factura.java
@@ -9,11 +9,13 @@ public class Factura {
         Scanner sc = new Scanner(System.in);
         float baseFactura = sc.nextFloat();
 
-        int totalFactura = baseFactura + ((baseFactura/100)*CANTIDAD_IVA);
+        float totalFactura = baseFactura + ((baseFactura/100)*CANTIDAD_IVA);
 
         System.out.println("El total de factura es " + totalFactura + " con un IVA del " + CANTIDAD_IVA);
 
         String cantidad = "22";
+
+        System.out.println("DESDE MI EQUIPO");
         
         
 
